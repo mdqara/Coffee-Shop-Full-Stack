cfd4aad81579a4772f449a124d398b1f3ca08aa2
Mohammad Qara <mdcr55@gmail.com>
2020-07-18 19:12:59 +0300

    added login URL

--- backend/src/api.py
+++ backend/src/api.py
@@ -18,110 +18,180 @@ CORS(app)
 '''
 # db_drop_and_create_all()
 
-## ROUTES
-'''
-@TODO implement endpoint
+
+# ROUTES
+
+
+@app.route('/drinks', methods=['GET'])
+def get_drinks(*args, **kwargs):
+    '''
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
-'''
+    '''
+    try:
+        drinks = list(map(Drink.short, Drink.query.all()))
+        return jsonify({
+            "success": True,
+            "drinks": drinks
+        })
+    except:
+        abort(500)
 
-@app.route('/drinks', methods=['GET'])
-def get_drinks(*args, **kwargs):
-    drinks = list(map(Drink.short, Drink.query.all()))
-    return jsonify({
-        "success": True,
-        "drinks": drinks
-    })
 
-
-'''
-@TODO implement endpoint
+@app.route('/drinks-detail', methods=['GET'])
+@requires_auth('get:drinks-detail')
+def get_drinks_detail(*args, **kwargs):
+    '''
     GET /drinks-detail
         it should require the 'get:drinks-detail' permission
         it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
-'''
+    '''
+    try:
+        drinks = list(map(Drink.long, Drink.query.all()))
+        return jsonify({
+            "success": True,
+            "drinks": drinks
+        })
+    except:
+        abort(500)
 
-@app.route('/drinks-detail', methods=['GET'])
-@requires_auth('get:drinks-detail')
-def get_drinks_detail(*args, **kwargs):
-    drinks = list(map(Drink.long, Drink.query.all()))
-    return jsonify({
-        "success": True,
-        "drinks": drinks
-    })
 
-
-'''
-@TODO implement endpoint
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def post_drink():
+    '''
     POST /drinks
         it should create a new row in the drinks table
         it should require the 'post:drinks' permission
         it should contain the drink.long() data representation
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
         or appropriate status code indicating reason for failure
-'''
+    '''
+    try:
+        data = request.get_json()
+        title = data.get('title', None)
+        recipe = data.get('recipe', None)
+
+        drink_to_post = Drink(title=title, recipe=json.dumps(recipe))
+        drink_to_post.insert()
+
+        return jsonify({
+            "success": True,
+            'drinks': drink_to_post.long()
+        })
+    except Exception:
+        abort(422)
 
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks/<int:id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def patch_drinks(*args, **kwargs):
+    '''
+   PATCH /drinks/<id>
+       where <id> is the existing model id
+       it should respond with a 404 error if <id> is not found
+       it should update the corresponding row for <id>
+       it should require the 'patch:drinks' permission
+       it should contain the drink.long() data representation
+   returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
+       or appropriate status code indicating reason for failure
+   '''
+    drink_id = kwargs.get("drink_id", None)
+    if drink_id is None:
+        abort(400)
+
+    data = request.get_json()
+    title = data.get('title', None)
+    recipe = data.get('recipe', None)
+
+    drink = Drink.query(drink_id)
+
+    if drink is None:
+        abort(404)  # drink not found
+    if title is None:
+        abort(400)
+
+    try:
+        drink.title = title
+        drink.recipe = json.dumps(recipe)
+        drink.update()
+        return jsonify({
+            "success": True,
+            "drinks": [drink.long()]
+        })
+    except Exception:
+        abort(422)
 
 
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks/<int:id>', methods=['DELETE'])
+def delete_drink(*args, **kwargs):
+    '''
+        DELETE /drinks/<id>
+            where <id> is the existing model id
+            it should respond with a 404 error if <id> is not found
+            it should delete the corresponding row for <id>
+            it should require the 'delete:drinks' permission
+        returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
+            or appropriate status code indicating reason for failure
+    '''
+
+    drink_id = kwargs.get("drink_id", None)
+    if drink_id is None:
+        abort(400)
+
+    drink = Drink.query(drink_id)
+    if drink is None:
+        abort(404)
+    try:
+        drink.delete()
+        return jsonify({
+            "success": True,
+            "delete": drink_id
+        })
+    except:
+        abort(422)
 
 
-## Error Handling
-'''
-Example error handling for unprocessable entity
-'''
+# Error Handling
 @app.errorhandler(422)
 def unprocessable(error):
+    try:
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "Unprocessable Entity"
+        }), 422
+    except:
+        abort(500)
+
+
+@app.errorhandler(404)
+def not_found(error):
     return jsonify({
-                    "success": False, 
-                    "error": 422,
-                    "message": "unprocessable"
-                    }), 422
-
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False, 
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
-'''
-
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above 
-'''
+        "success": False,
+        "error": 404,
+        "message": "Resource is not found"
+    }), 404
 
 
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above 
-'''
+@app.errorhandler(401)
+def unauthorized(error):
+    return jsonify({
+        "success": False,
+        "error": 401,
+        "message": "Unauthorized Access"
+    }), 401
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "Bad Request"
+    }), 400
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,11 +4,11 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
+  apiServerUrl: "http://127.0.0.1:5000", // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
-  }
+    url: "dev-xp3i9c1n.us", // the auth0 domain prefix
+    audience: "https://dev-xp3i9c1n.us.auth0.com/api/v2/", // the audience set for the auth0 app
+    clientId: "ZQ1pfeSK6mIfm7NhaP5HnaNq51GGPDqn", // the client id generated for the auth0 app
+    callbackURL: "http://localhost:8100", // the base url of the running ionic application.
+  },
 };
